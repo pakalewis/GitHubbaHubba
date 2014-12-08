8869b6fe08048c4e5edb3655813e0cf05872390d
Parker <pakalewis@gmail.com>
2014-12-08 11:40:54 -0800

    Move project to new repo

---
+++ GithubbaHubba/UserCell.swift
@@ -0,0 +1,31 @@
+//
+//  UserCell.swift
+//  GithubbaHubba
+//
+//  Created by Parker Lewis on 10/22/14.
+//  Copyright (c) 2014 Parker Lewis. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UICollectionViewCell {
+  
+    
+    
+    @IBOutlet weak var userAvatarImageView: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.userAvatarImageView.image = nil
+        self.userName.text = nil
+        
+    }
+    
+    
+}
